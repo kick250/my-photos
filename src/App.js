940caf9caec10ba3a8d69bca78129aea7ca21318
kick250 <kicklobal@gmail.com>
2022-09-25 20:07:30 -0300

    permitir deletar e favoritar imagem

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ class App extends React.Component{
           createdAt: new Date(),
         },
         {
-          name: 'test',
+          name: 'test 2',
           photoLink: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/apple-tv-4k-hero-select-202104_FMT_WHH?wid=640&hei=600&fmt=jpeg&qlt=90&.v=1617137945000',
           description: 'urso bonito',
           favorited: true,
-          id: Date.now(),
+          id: Date.now() + 40,
           createdAt: new Date(),
         }
       ]
@@ -34,7 +34,8 @@ class App extends React.Component{
         <p className="h1 py-3 text-center header">My Photos</p>
         <div className="mx-3">
           <PhotoForm onSavedPhoto={photo => this.createPhoto(photo)}/>
-          <div className="row m-5">
+          <div className="row m-5 gap-5">
+            <div className="col-12 h2 text-center mb-5">Imagens adicionadas</div>
             {this.showPhotosList()}
           </div>
         </div>
@@ -46,12 +47,12 @@ class App extends React.Component{
     const photos = this.state.photos
 
     if (photos.length === 0)
-      return (<div style={{display: 'none'}}></div>);
+      return (<div className="h2 text-center">Nenhuma imagem adicionada</div>);
 
     return (
       photos.map((photo, index) => {
         return (
-          <div key={index} className="col-12 col-sm-6 col-lg-6 row">
+          <div key={index} className="photo-box col-12 col-sm-6 col-lg-6 row">
             <div className="col-12 col-lg-8 col">
               <img className="photo" src={photo.photoLink} alt={photo.name} title={photo.name}></img>
             </div>
@@ -63,8 +64,10 @@ class App extends React.Component{
                 </div>
                 <p>Criada em: {photo.createdAt.toLocaleDateString('pt-BR')}</p>
               </div>
-              <div>
-                {photo.favorited ? (<p className="star">&#9733;</p>) : ''}
+              <div className="d-flex align-items-center gap-2 justify-content-end">
+                <button onClick={() => this.deletePhoto(photo)} className="btn btn-danger">&#x1F5D1;</button>
+                <button className="btn btn-primary">&#128190;</button>
+                {photo.favorited ? (<button onClick={() => this.favorite(photo)} className="star btn">&#9733;</button>) : (<button onClick={() => this.favorite(photo)} className="star btn">&#9734;</button>)}
               </div>
             </div>
           </div>
@@ -78,6 +81,31 @@ class App extends React.Component{
     photos.push(
       photo
     );
+    this.setState({photos});
+  }
+
+  deletePhoto(photo) {
+    let photos = this.state.photos;
+    photos = photos.filter(statePhoto => {
+      return (photo.id !== statePhoto.id)
+    });
+    this.setState({photos});
+  }
+
+  favorite(photo) {
+    photo.favorited = !photo.favorited;
+    this.save(photo);
+  }
+
+  save(photo) {
+    let photos = this.state.photos;
+    photos = photos.map(statePhoto => {
+      if (photo.id === statePhoto.id)
+        return photo;
+
+      return statePhoto;
+
+    });
     this.setState({photos});
   }
 }
