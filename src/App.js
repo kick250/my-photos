015d45687784eec0e467447689892f66c2c5ebac
kick250 <kicklobal@gmail.com>
2022-09-25 18:47:58 -0300

    permitir adicionar imagem

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import React from "react";
 import PhotoForm from "./components/PhotoForm.js";
+import "./app.css";
 
 class App extends React.Component{
   constructor(props) {
     super(props);
 
     this.state = {
-      photos: []
+      photos: [
+        {
+          name: 'test',
+          photoLink: 'https://www.eusemfronteiras.com.br/wp-content/uploads/2021/05/pexels-rasmus-svinding-35435-810x537.jpg',
+          description: 'urso bonito',
+          favorited: false,
+          id: Date.now(),
+          createdAt: new Date(),
+        },
+        {
+          name: 'test',
+          photoLink: 'https://static3.depositphotos.com/1000608/113/i/600/depositphotos_1131732-stock-photo-brown-bear-and-a-bird.jpg',
+          description: 'urso bonito',
+          favorited: true,
+          id: Date.now(),
+          createdAt: new Date(),
+        }
+      ]
     };
   }
 
   render() {
     return (
       <div>
-        <p className="h1 my-3 text-center">My Photos</p>
-        <PhotoForm/>
+        <p className="h1 py-3 text-center header">My Photos</p>
+        <div className="mx-3">
+          <PhotoForm onSavedPhoto={photo => this.createPhoto(photo)}/>
+          <div className="row">
+            {this.showPhotosList()}
+          </div>
+        </div>
       </div>
     );
+  }
+
+  showPhotosList() {
+    const photos = this.state.photos
+
+    if (photos.length === 0)
+      return (<div style={{display: 'none'}}></div>);
+
+    return (
+      photos.map((photo, index) => {
+        return (
+          <div key={index} className="col-12 row">
+
+          </div>
+        )
+      })
+    );
+  }
+
+  createPhoto(photo) {
+    let photos = this.state.photos;
+    photos.push(
+      photo
+    );
+    this.setState({photos});
   }
 }
 
--- src/components/PhotoForm.js
+++ src/components/PhotoForm.js
@@ -3,11 +3,111 @@ import React from "react";
 export default class PhotoForm extends React.Component{
   constructor(props) {
     super(props);
+
+    this.state = {
+      name: '',
+      favorited: false,
+      photoLink: '',
+      description: '',
+      errors: []
+    };
   }
 
   render() {
     return (
-      <h1>hello</h1>
+      <div>
+        <p className="h2 text-center">Adicionar imagem</p>
+        <div className="row justify-content-center align-items-center gap-3">
+          {this.showErrors()}
+          <div className="col-8">
+            <input onInput={event => this.nameBind(event)} type={'text'} className="form-control text-center" placeholder="Nome"/>
+          </div>
+          <div className="col-12 col-sm-2 col-lg-2 form-check d-flex justify-content-center gap-2">
+            <input className="form-check-input" onInput={event => this.favoritedBind(event)} type={'checkbox'}/>
+            <label className="form-check-label">Favoritar</label>
+          </div>
+          <div className="col-12">
+            <input onInput={event => this.photoLinkBind(event)} type={'text'} className="form-control text-center" placeholder="link da imagem"/>
+          </div>
+          <div className="col-12">
+            <textarea onInput={event => this.descriptinoBind(event)} className="form-control" cols={30} rows={5} placeholder="Descrição"></textarea>
+          </div>
+          <button className="btn btn-primary add-button col-6" onClick={() => this.save()} type="button">Salvar</button>
+        </div>
+      </div>
+    );
+  }
+
+  nameBind({target}) {
+    const name = target.value;
+    this.setState({name});
+  }
+  favoritedBind() {
+    const favorited = !this.state.favorited;
+    this.setState({favorited});
+  }
+  photoLinkBind({target}) {
+    const photoLink = target.value;
+    this.setState({photoLink});
+  }
+  descriptinoBind({target}) {
+    const description = target.value;
+    this.setState({description});
+  }
+
+  showErrors() {
+    const errors = this.state.errors;
+
+    if (errors.length === 0)
+      return (<div style={{display: 'none'}}></div>);
+
+    return (
+      errors.map((error, index) => {
+        return (
+          <div className="alert alert-danger" key={index}>
+            {error}
+          </div>
+        )
+      })
     )
+  }
+
+  save() {
+    const photo = {
+      name: this.state.name,
+      photoLink: this.state.photoLink,
+      description: this.state.description,
+      favorited: this.state.favorited,
+      id: Date.now(),
+      createdAt: new Date(),
+    };
+
+    if (!this.validate(photo)) return;
+
+    this.props.onSavedPhoto(photo);
+  }
+
+  validate(photo) {
+    const errors = [];
+
+    if (photo.name.length <= 4)
+      errors.push('O nome da imagem deve ter pelo menos 5 digitos.');
+
+    if (photo.description.length <= 9)
+      errors.push('A descrição da imagem deve ter pelo menos 10 digitos.');
+
+    if (!this.verifyUrl(photo.photoLink))
+      errors.push('A url da imagem deve ser valida');
+
+    this.setState({errors});
+
+    return (errors.length === 0);
+  }
+
+  verifyUrl(url) {
+    // eslint-disable-next-line
+    const r = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+
+    return r.test(url);
   }
 }
