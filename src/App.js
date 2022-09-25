187f6fc977f1c74a94b4930e61f234a611a71def
kick250 <kicklobal@gmail.com>
2022-09-25 16:17:08 -0300

    comecar projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PhotoForm from "./components/PhotoForm.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      photos: []
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p className="h1 my-3 text-center">My Photos</p>
+        <PhotoForm/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/PhotoForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class PhotoForm extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>hello</h1>
+    )
+  }
+}
